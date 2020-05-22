64b07392d1fbfbce2f0cc0bc7cbd4380ef9cbf53
Somayeh Zamiri <65766183+SomayehZamiri@users.noreply.github.com>
2020-05-22 17:52:08 +0430

    The code for cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## this function gets a matrix and computes its inverse and chashes it in y.
 
 makeCacheMatrix <- function(x = matrix()) {
+      y <<- solve(x)
+  }
 
-}
 
-
-## Write a short comment describing this function
+## this function gets a matrix and if its inverse was already chased in the memory,
+## returns it, if not, calculates it and gives the inversed matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+      if(!is.null(y)) {
+      message("getting cached data")
+      return(y)
+    }
+    else {
+      z <- solve (x)
+      print (z)
+    }
+  }
+      
